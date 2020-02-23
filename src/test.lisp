6d0583d6145aa4159cf86ca025809f10b9f75c8f
lagagain <lagagain@email.com>
2020-02-23 17:34:03 +0800

    update: change test-function to test case (with test framwork)

--- src/test.lisp
+++ src/test.lisp
@@ -1,59 +1,65 @@
-(in-package cl.lag-markdown)
+(defpackage cl.lag-markdown/test
+  (:use :cl :cl.lag-markdown :fiveam))
 
+(in-package cl.lag-markdown/test)
 
-(string= (to-markdown (make-instance 'text-line
-                                     :text "Hello"
-                                     :newline nil))
-         "Hello")
-
-(string= (to-markdown (make-instance 'text-line
-                                     :text "Hello"
-                                     :newline t))
-         "Hello
-")
-
-(string= "# Title #
-"
-         (to-markdown (make-instance 'header
-                                     :name "Title"
-                                     :level 1)))
-
-(string= (format nil "# Title #
-Hello, World  ~&")
-         (to-markdown (make-instance 'header
-                                     :name "Title"
-                                     :level 1
-                                     :content `(,(make-instance 'text-line
-                                                                :text "Hello, "
-                                                                :newline nil)
-                                                 ,(make-instance 'text-line
-                                                                 :text "World"
-                                                                 :newline t)))))
-(string= (format nil  "# Title #
+(test to-markdown
+  (is-true (string= (to-markdown (make-instance 'text-line
+                                                :text "Hello"
+                                                :newline nil))
+                    "Hello"))
+  (is-true (string= (to-markdown (make-instance 'text-line
+                                                :text "Hello"
+                                                :newline t))
+                    (format nil "Hello  ~&")))
+  (is-true
+   (string= (to-markdown (make-instance 'header
+                                        :name "Title"
+                                        :level 1))
+            "# Title #
+"))
+  (is-true
+   (string= (to-markdown (make-instance 'header
+                                        :name "Title"
+                                        :level 1
+                                        :content `(,(make-instance 'text-line
+                                                                   :text "Hello, "
+                                                                   :newline nil)
+                                                   ,(make-instance 'text-line
+                                                                   :text "World"
+                                                                   :newline t))))
+            (format nil "# Title #
+Hello, World  ~&")))
+  (is-true
+   (string= (to-markdown (make-instance 'header
+                                        :name "Title"
+                                        :level 1
+                                        :content `(,(make-instance 'text-line
+                                                                   :text "Hello, "
+                                                                   :newline nil)
+                                                   ,(make-instance 'text-line
+                                                                   :text "World"
+                                                                   :newline t)
+                                                   ,(make-instance 'text-line
+                                                                   :text "Bye Bye"
+                                                                   :newline nil))))
+            (format nil  "# Title #
 Hello, World  ~&Bye Bye
-")
-         (to-markdown (make-instance 'header
-                                     :name "Title"
-                                     :level 1
-                                     :content `(,(make-instance 'text-line
-                                                                :text "Hello, "
-                                                                :newline nil)
-                                                 ,(make-instance 'text-line
-                                                                 :text "World"
-                                                                 :newline t)
-                                                 ,(make-instance 'text-line
-                                                                 :text "Bye Bye"
-                                                                 :newline nil)))))
-(string= (format nil "# Title #
+")))
+  (is-true
+   (string= (to-markdown (make-instance 'header
+                                        :name "Title"
+                                        :level 1
+                                        :content `(,(make-instance 'text-line
+                                                                   :text "Hello, "
+                                                                   :newline nil)
+                                                   ,(make-instance 'text-line
+                                                                   :text "World"
+                                                                   :newline t)
+                                                   "Bye Bye")))(format nil "# Title #
 Hello, World  ~&Bye Bye
-")
-         (to-markdown (make-instance 'header
-                                     :name "Title"
-                                     :level 1
-                                     :content `(,(make-instance 'text-line
-                                                                :text "Hello, "
-                                                                :newline nil)
-                                                 ,(make-instance 'text-line
-                                                                 :text "World"
-                                                                 :newline t)
-                                                 "Bye Bye"))))
+"))))
+
+
+(run!)
+
